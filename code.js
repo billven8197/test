15f67159dd33bf2d6536296ad0e96ce80c3c72a3
billven8197 <jacob@billven.se>
2016-09-26 16:32:34 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,10 @@
+function start(){
+
+    circle(random(500),random(500), random(150 /*50*/), "blue");
+    circle(random(500),random(500), random(150 /*50*/), "green");
+    circle(random(500),random(500), random(150 /*50*/), "red");
+    circle(random(500),random(500), random(150 /*50*/), "pink");
+
+
+
+}
